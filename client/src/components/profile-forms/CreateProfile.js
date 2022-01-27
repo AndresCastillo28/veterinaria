128890d23e0c7a3b69f4e1a78adc2887297e6574
AndresCastillo28 <ansa2000@hotmail.es>
2022-01-27 12:17:00 -0500

    avances

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import {Link, withRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = (props) => {
+const CreateProfile = ({createProfile, history}) => {
 
   const [formData, setFormData] = useState({
       name: '',
@@ -21,9 +23,72 @@ const CreateProfile = (props) => {
     breed
   } = formData;
 
-  return <div></div>;
+  const onChange = e => 
+    setFormData({ ...formData, [e.target]: e.target.value });
+
+  const onSubmit = e => {
+      e.preventDefault();
+      createProfile(formData, history);
+    }
+
+  return (
+  <div>
+
+    <form className='form' onSubmit={e => onSubmit(e)}>
+      <label className="form-label fs-5">Name</label>
+        <input
+        type="text"
+        className="form-control"
+        name='name'
+        value={name}
+        onChange={e => onChange(e)}
+        required
+        />
+      <label className="form-label fs-5">Weight</label>
+      <input
+        type="text"
+        className="form-control"
+        name='name'
+        value={weight}
+        onChange={e => onChange(e)}
+        required
+      />
+      <label className="form-label fs-5">Vaccines</label>
+      <input
+        type="text"
+        className="form-control"
+        name='name'
+        value={vaccines}
+        onChange={e => onChange(e)}
+        required
+      />
+      <label className="form-label fs-5">Dissases</label>
+      <input
+        type="text"
+        className="form-control"
+        name='name'
+        value={dissases}
+        onChange={e => onChange(e)}
+        required
+      />
+      <label className="form-label fs-5">Breed</label>
+      <input
+        type="text"
+        className="form-control"
+        name='name'
+        value={breed}
+        onChange={e => onChange(e)}
+        required
+      />
+    </form>
+  </div>
+  );
 };
 
 CreateProfile.propTypes = {};
 
-export default CreateProfile;
+const mapStateToProps = state => {
+  createProfile: PropTypes.func.isRequired
+}
+
+export default connect(null, { createProfile })(withRouter(CreateProfile));
